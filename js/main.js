1900ea4b635cbaf9994e25bd4477591c0ed17206
matbrady <mat.brady@threespot.com>
2013-06-18 16:45:15 -0400

    cleaning comments, and development variable for cache busting

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 (function() {
+  'strict';
+
+  /**
+   * RequireJS Configuration
+   *
+   * Contains configuration controls for both development 
+   * and production 
+   */
+
+  // Sets Global NoCache 
+  REQUIRE_NOCACHE = window.REQUIRE_NOCACHE || false;
 
   // Name libraries / plugins
   var config = {
@@ -11,15 +22,15 @@
      *     use a define method. ie non AMD compliant libraries 
      * - waitSeconds : Timeout for loading scripts
      */ 
+    enforceDefine: true,
 
-    // enforceDefine: true,
-
-    // waitSeconds: 200,
-
+    waitSeconds: 200,
     
     /** 
      * PRODUCTION 
      */ 
+    urlArgs: !!REQUIRE_NOCACHE ? "bust="+(new Date()).getTime() : '',
+
     paths: {
       "poly": "polyfills",
       // "backbone": "lib/backbone",
@@ -32,50 +43,51 @@
 
   };
 
-
-  // Search global for a "REQUIRE_NOCACHE" flag to enable cache-busting:
-  if (typeof REQUIRE_NOCACHE != 'undefined' && REQUIRE_NOCACHE) {
-    config.urlArgs = "bust="+(new Date()).getTime();
-  }
-
   // Predefine Require setup, or configure existing library:
   if (typeof require == 'undefined') {
     require = config;
-  } else if (typeof requirejs == 'function') {
+  } 
+  else if (typeof requirejs == 'function') {
     requirejs.config(config);
   }
+
+  /* End RequireJS Configuration */
+
+
+
+  /**
+   * Application Setup and Bootstrap
+   *
+   * @require
+   *   detection : non-jquery id/class detection
+   *
+   */
+  define(['poly/detection'], function( detection ) {
+
+    function setup() {
+        var mods = [];
+        
+        if (!window.JSON) {      // << test for native JSON support.
+            mods.push('poly/json3');  // << fill in with shim if missing.
+        }
+        
+        require(mods, bootstrap);
+    };
+
+    function bootstrap() {
+        var mods = [];
+
+        if ( detection.has('#main') ) {
+            console.log("We've go a MAIN div");
+            // mods.push('common/tabs');
+        }
+
+        require(mods);
+    }
+
+    setup();
+
+
+  });
   
 }());
-
-/**
- * Application Setup
- *
- * 
- */
-define(['poly/detection'], function( detection ) {
-
-	function setup() {
-      var mods = [];
-      
-      if (!window.JSON) {      // << test for native JSON support.
-          mods.push('poly/json3');  // << fill in with shim if missing.
-      }
-      
-      require(mods, bootstrap);
-  };
-
-  function bootstrap() {
-      var mods = [];
-
-      if ( detection.has('#main') ) {
-          console.log("We've go a MAIN div");
-          // mods.push('common/tabs');
-      }
-
-      require(mods);
-  }
-
-  setup();
-
-
-});
