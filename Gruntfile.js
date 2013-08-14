3af6a343e1309d5ea7bcd1199733729a71aa43f4
matbrady <mat.brady@threespot.com>
2013-08-14 09:07:50 -0400

    including livereload version

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,66 +1,74 @@
 module.exports = function( grunt ) {
 
-	grunt.initConfig({
+  grunt.initConfig({
 
-		pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('package.json'),
 
-		config: {
+    config: {
 
-			app: 'app/public/',
-			dist: {
-				debug: 'dist/debug',
-				release: 'dist/release'
-			},
-			port: 3000
-		},
+        app: 'app/public/',
+        dist: {
+            debug: 'dist/debug',
+            release: 'dist/release'
+        },
+        port: 3000
+    },
 
-		// VALIDATION
-		// Run the application Javascript through JSHint with the defaults.
-		jshint: {
-      files: [
-	      '<%= config.app%>/js/**/*.js'
-      ],
+    // VALIDATION
+    // Run the application Javascript through JSHint with the defaults.
+    jshint: {
+      files: ['<%= config.app%>/js/**/*.js'],
       options: {
           ignores: [
-          	'<%= config.app%>js/polyfills/*',
-          	'<%= config.app%>js/vendor/*',
-          	'<%= config.app%>js/lib/*'
-          ],
+            '<%= config.app%>js/polyfills/*',
+            '<%= config.app%>js/vendor/*',
+            '<%= config.app%>js/lib/*'
+          ]
       }
     },
 
     requirejs: {
-    	release: {
-    		options: {
-    			name: 'main',
-    			baseUrl: '<%= config.app %>js/',
-    			mainConfigFile: '<%= config.app %>js/config.js',
-    			// insertRequire: ['main'],
-    			paths: {
-    				'poly': 'polyfills'
-    			},
-    			out: 'dist/js/<%= pkg.name %>.js'
-    		}
-    	}
+        release: {
+            options: {
+                name: 'main',
+                baseUrl: '<%= config.app %>js/',
+                mainConfigFile: '<%= config.app %>js/config.js',
+                // insertRequire: ['main'],
+                paths: {
+                    'poly': 'polyfills'
+                },
+                out: 'dist/js/<%= pkg.name %>.js'
+            }
+        }
     },
 
     useminPrepare: {
-        html: ['dist/index.html']
+        dev: {
+          html: ['app/public/index.html']
+        },
+        build: {
+          html: ['dist/index.html']
+        }
     },
 
     usemin: {
-        html: ['dist/index.html']
+        dev: {
+          html: ['app/public/index.html']
+        },
+        build: {
+          html: ['dist/index.html']
+        }
     },
 
     clean: {
-    	release: {
-    		src: ['dist']
-    	}
+        release: {
+            src: ['dist']
+        }
     },
 
     copy: {
       release: {
-      	expand: true,
+        expand: true,
         src: ['**/*', '!js/**', 'js/lib/**'],
         cwd: '<%= config.app %>',
         dest: 'dist/'
@@ -70,100 +78,101 @@ module.exports = function( grunt ) {
     },
 
     express: {
-    	dev: {
-    		options: {
-    			script: 'app/server.js',
-    			background: false, // prevents server from closing
-    		}
-    	},
-    	prod: {
-    		options: {
-    			script: 'app/server.js',
-    			node_env: 'production'
-    		}
-    	}
+        dev: {
+            options: {
+                script: 'app/server.js'
+                // background: false, // prevents server from closing
+            }
+        },
+        prod: {
+            options: {
+                script: 'app/server.js',
+                node_env: 'production'
+            }
+        }
     },
 
     open: {
-    	dev: {
-    		path: 'http://127.0.0.1:<%= config.port %>',
-    		app: 'Google Chrome'
-    	}
+        dev: {
+            path: 'http://127.0.0.1:<%= config.port %>',
+            app: 'Google Chrome'
+        }
     },
 
     watch: {
-    	scripts: {
-    		files: ['<%= config.app %>js/**/*.js'],
-    		tasks: ['jshint'],
-    		options: {
-    			livereload: true
-    		}
-    	}
-    },
-
-    nodemon: {
-      prod: {
-        options: {
-          file: 'app/server.js',
-          args: ['development'],
-          nodeArgs: ['--debug']
+        // scripts: {
+        //  files: ['<%= config.app %>js/**/*.js'],
+        //  tasks: ['jshint'],
+        //  options: {
+        //      livereload: true
+        //  }
+        // }
+        express: {
+            files: ['app/public/**'],
+            tasks: ['express:dev'],
+            options: {
+                livereload: true,
+                nospawn: true
+            }
         }
-      }
     }
-	});
-
-    // Confirmed
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-requirejs');
-	grunt.loadNpmTasks('grunt-usemin');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-express-server');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-
-    // Potential
-	grunt.loadNpmTasks('grunt-open');
-    grunt.loadNpmTasks('grunt-nodemon');
-
-	grunt.registerTask('default', [
-		'jslint'
-	]);
+  });
 
 
-	// SERVERS
+  // NPM Tasks ================================================>
+  // Confirmed
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-express-server');
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
-	/** Server - DEV
-	 * 
-	 */
-	grunt.registerTask('server', [
-		'open',
-		'express:dev'
-	]);	
+  // Potential
+  grunt.loadNpmTasks('grunt-open');
 
-	// BUILDS
 
-	/** Build - Standard
-	 * Creates a standard build with no influence from any BE
-	 * asset pipeline
-	 * - 'clean': clears all content from the config.dist.??? directory
-	 * - 'copy:release':
-	 * - 'userminPrepare'	
-	 * - 'usemin'
-	 * - 'requiresjs': // BUG?! Forced to call requirejs again because the output file is not being generated
-	 */
-	grunt.registerTask('build', [
-		'clean', 
-		'copy:release',
-		'useminPrepare', 
-		'usemin', 
-		'requirejs'
-	]);
+  // Registered Tasks =========================================>
+  grunt.registerTask('default', [
+      'jslint'
+  ]);
 
-	/**	Build - Rails	
-	 * Creates a build for easy integration into a Rails BE
-	 */
-	grunt.registerTask('build:rails', [
-		'copy:rails'
-	]);
+  // SERVERS
+
+  /** Server - DEV
+   * 
+   */
+  grunt.registerTask('server', [
+      'open',
+      'express:dev',
+      'watch'
+  ]); 
+
+  // BUILDS
+
+  /** Build - Standard
+   * Creates a standard build with no influence from any BE
+   * asset pipeline
+   * - 'clean': clears all content from the config.dist.??? directory
+   * - 'copy:release':
+   * - 'userminPrepare'   
+   * - 'usemin'
+   * - 'requiresjs': // BUG?! Forced to call requirejs again because the output file is not being generated
+   */
+  grunt.registerTask('build', [
+      'clean', 
+      'copy:release',
+      'useminPrepare', 
+      'usemin', 
+      'requirejs'
+  ]);
+
+  /** Build - Rails   
+   * Creates a build for easy integration into a Rails BE
+   */
+  grunt.registerTask('build:rails', [
+      'copy:rails'
+  ]);
 };
--- app/public/js/config.js
+++ app/public/js/config.js
@@ -6,49 +6,49 @@
  */
 (function() {
 
-	// Sets Global NoCache 
-	REQUIRE_NOCACHE = window.REQUIRE_NOCACHE || false;
+  // Sets Global NoCache 
+  REQUIRE_NOCACHE = window.REQUIRE_NOCACHE || false;
 
-	// Name libraries / plugins
-	var config = {
+  // Name libraries / plugins
+  var config = {
 
-	  /** 
-	   * DEVELOPMENT 
-	   *
-	   * The below lines should be commented before pushing to production
-	   * - enforceDefine : causes error when modules are loaded that don't
-	   *     use a define method. ie non AMD compliant libraries 
-	   * - waitSeconds : Timeout for loading scripts
-	   */ 
-	  enforceDefine: true,
+    /** 
+     * DEVELOPMENT 
+     *
+     * The below lines should be commented before pushing to production
+     * - enforceDefine : causes error when modules are loaded that don't
+     *     use a define method. ie non AMD compliant libraries 
+     * - waitSeconds : Timeout for loading scripts
+     */ 
+    enforceDefine: true,
 
-	  waitSeconds: 200,
-	  
-	  /** 
-	   * PRODUCTION 
-	   */ 
-	  urlArgs: !!REQUIRE_NOCACHE ? "bust="+(new Date()).getTime() : '',
+    waitSeconds: 200,
+    
+    /** 
+     * PRODUCTION 
+     */ 
+    urlArgs: !!REQUIRE_NOCACHE ? "bust="+(new Date()).getTime() : '',
 
-	  paths: {
-	    "poly": "polyfills"
-	    // "backbone": "lib/backbone",
-	    // "jquery": "lib/jquery",
-	    // "raphael": "lib/raphael",
-	    // "require": "lib/require",
-	    // "underscore": "lib/underscore",
-	    // "usMap": "lib/usMap"
-	  }
+    paths: {
+      "poly": "polyfills"
+      // "backbone": "lib/backbone",
+      // "jquery": "lib/jquery",
+      // "raphael": "lib/raphael",
+      // "require": "lib/require",
+      // "underscore": "lib/underscore",
+      // "usMap": "lib/usMap"
+    }
 
-	};
+  };
 
-	// Predefine Require setup, or configure existing library:
-	if (typeof require == 'undefined') {
-	  require = config;
-	} 
-	else if (typeof requirejs == 'function') {
-	  requirejs.config(config);
-	}
+  // Predefine Require setup, or configure existing library:
+  if (typeof require == 'undefined') {
+    require = config;
+  } 
+  else if (typeof requirejs == 'function') {
+    requirejs.config(config);
+  }
 
-	/* End RequireJS Configuration */
+  /* End RequireJS Configuration */
 
 }());
